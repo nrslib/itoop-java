88e38eb29ceb3983261aa308e99bb1f22e93f748
nrslib <masanobu.naruse@gmail.com>
2021-08-02 20:27:16 +0900

    fix invalid import

--- src/src/base/_06/JankenGame.java
+++ src/src/base/_06/JankenGame.java
@@ -1,9 +1,5 @@
 package base._06;
 
-import sample._05.NormalRule;
-import sample._05.ReverseRule;
-import sample._05.Rule;
-
 class JankenGame {
     // Rule を引数で受け取るように書き換えよう
     void play(int player, int cpu, Display display, String ruleName) {
