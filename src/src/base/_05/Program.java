0d2bcaf28e6c6f796c930e30d499620d4447b282
nrslib <masanobu.naruse@gmail.com>
2021-11-11 15:33:16 +0900

    change 'normal' -> 'default'

--- src/src/base/_05/Program.java
+++ src/src/base/_05/Program.java
@@ -3,7 +3,7 @@ package base._05;
 public class Program {
     public static void main(String[] args) {
         var game = new JankenGame();
-        game.play(1, 2, "ja", "normal");
+        game.play(1, 2, "ja", "default");
         game.play(1, 2, "en", "reverse");
     }
 }
